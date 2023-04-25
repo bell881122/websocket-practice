5bd3dec886caf98d00924273b4ef1ca72d7fb48b
Emi <bell881122@gmail.com>
2023-04-25 22:18:26 +0800

    實作簡易時時聊天室

---
+++ README.md
@@ -0,0 +1,15 @@
+# Starting
+
+## Step1-Terminal
+```
+yarn install
+yarn dev
+```
+
+## Step2-Browser
+1. Open `http://localhost:8888`
+2. Open `http://localhost:8888` in another tab
+3. Now two windows can chat with each other
+
+# References
+[[WebSocket] 實作一個簡單的聊天室 (+ Node.js)](https://eudora.cc/posts/220105/)
--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const express = require('express');
+const express = require('express')
+const SocketServer = require('ws').Server
+const PORT = 8888
 
-const app = express();
+//創建 express 物件，綁定監聽  port , 設定開啟後在 console 中提示
+const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello! HOOKLOOP!!!');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '\\index.html');
 });
 
-app.listen(8088, () => {
-    console.log('Server is running!');
-});
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
+
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server })
+//當有 client 連線成功時
+wss.on('connection', ws => {
+    console.log('Client connected')
+    // 當收到client消息時
+    ws.on('message', data => {
+        // 收回來是 Buffer 格式、需轉成字串
+        data = data.toString()
+
+        // // 發送消息給當前 client 
+        // ws.send(data)
+
+        // 發送給所有client： 
+        let clients = wss.clients  //取得所有連接中的 client
+        clients.forEach(client => {
+            client.send(data)  // 發送至每個 client
+        })
+    })
+    // 當連線關閉
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
