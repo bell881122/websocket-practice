ebe8faf52b90faf22755b97a56cca693b49fe545
Emi <bell881122@gmail.com>
2023-04-25 21:37:59 +0800

    init project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello! HOOKLOOP!!!');
+});
+
+app.listen(8088, () => {
+    console.log('Server is running!');
+});
